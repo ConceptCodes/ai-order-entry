77efd4defe47dea2c8b206851fb0fcf42cc344ae
concept <conceptcodes@gmail.com>
2025-05-04 15:02:54 -0500

    refactor: Simplify audioInputNode return structure by removing Command and Nodes references

--- src/nodes/audio-input.ts
+++ src/nodes/audio-input.ts
@@ -1,8 +1,6 @@
 import { HumanMessage } from "@langchain/core/messages";
-import { Command } from "@langchain/langgraph";
 
 import { listenAndTranscribe } from "../helpers/utils";
-import { Nodes } from "../helpers/constants";
 import type {
   AgentStateAnnotation,
   ConfigurationAnnotation,
@@ -16,10 +14,7 @@ export const audioInputNode = async (
   const language = config.configurable?.language || "en";
   const transcript = await listenAndTranscribe(language);
 
-  return new Command({
-    goto: Nodes.PARSE_INTENT,
-    update: {
-      messages: [new HumanMessage(transcript)],
-    },
-  });
+  return {
+    messages: [new HumanMessage(transcript)],
+  };
 };
